e69bd7843a1d2be99b64695fe54bc9e8261c2ea4
rrose666 <uziolko@hotmail.com>
2021-03-23 12:27:56 +0000

    lists and tuples

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-## Data collections in Python
+## Data collections in Python:
+
+- Lists and Tuples
+- Dictionaries
+- Sets
+
+### What is a list?
+- Commonly used to store the data
+- Lists are MUTABLE
+- syntax [ ] used to create a list
+example:
+- ` shopping_list = ["bread", "chocolate", "avocados", "milk"]`
+- `print(shopping_list[1])`
+
+### To change a value in a list
+- `shopping_list[0] = "orange"`
+- `print(shopping_list)`
+
+### To add a new value to a list
+- `shopping_list.insert(1, "ice cream")`
+- `shopping_list.append("apple")`
+
+
+### To remove a value from a list
+- `shopping_list.remove("orange")`
+
+### pop() deletes the last item from the list
+- `shopping_list.pop()`
---
+++ lists&tuples.py
@@ -0,0 +1,32 @@
+# What is a list?
+# Commonly used to store the data
+# Lists are MUTABLE
+# syntax [] used to create a list
+
+# shopping_list = ["bread", "chocolate", "avocados", "milk"]
+#                  #  0         1            2          3
+# print(type(shopping_list))
+# print(shopping_list)
+# print(shopping_list[1])
+# print(shopping_list[-2])
+# # to change a value in a list
+# shopping_list[0] = "orange"
+# print(shopping_list)
+#
+# # To add to the list
+# shopping_list.insert(1, "ice cream")
+# shopping_list.append("apple")
+# print(shopping_list)
+#
+# # To remove an item from a list
+# shopping_list.remove("orange")
+# print(shopping_list)
+
+# pop() deletes the last item from the list
+# shopping_list.pop()
+
+# we can mix data types in a list
+mixed_list = [1, 2, 3, "one", "two", "three"]
+print(mixed_list[1:3])
+
+
